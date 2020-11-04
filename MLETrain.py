c64572971604fc609cfdc362be9533feba183ae2
Roi Peretz <roip890@gmail.com>
2020-11-04 12:15:02 +0200

    split to tokens

--- MLETrain.py
+++ MLETrain.py
@@ -1,2 +1,9 @@
-path = ''
+path = 'data/ass1-tagger-train'
 
+with open(path) as content_file:
+    content = content_file.read()
+    tokens = content.split(' ')
+    for token in tokens:
+        if '/' in token and len(token.split('/')) == 2:
+            [word, tag] = token.split('/')
+            print(word, ':', tag)
